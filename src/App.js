8f09695079b8c884e6bba494376546393cf65122
VovkAndriy <andriy.vovk.work@gmail.com>
2020-12-22 10:13:04 +0200

    initial test commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React fff
         </a>
       </header>
     </div>
